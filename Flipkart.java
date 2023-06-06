d7c2ddae30462bb6dd3f761221f1133b7c5ef011
shubhamlole <loleshubham46@gmail.com>
2023-06-06 10:23:17 +0530

    sample java file

---
+++ Flipkart.java
@@ -0,0 +1,14 @@
+// This is a simple Java program.
+// FileName : "HelloWorld.java".
+
+class Flipkart
+{
+	// Your program begins with a call to main().
+	// Prints "Hello, World" to the terminal window.
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Devops world");
+		System.out.println("welcome to jenkins");
+	}
+}
+
